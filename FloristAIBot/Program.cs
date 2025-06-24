937130ed6497c9b6d82f653f40d99cf669259f5c
downh <downh@DESKTOP-AR9ARAH>
2025-06-24 23:41:54 +0300

    Настройка заглушки для тестового хоста

--- FloristAIBot/Program.cs
+++ FloristAIBot/Program.cs
@@ -1,49 +1,11 @@
-using Adapter;
-using FloristAI.Application;
-using FloristAI.Infrastructure;
-using Microsoft.EntityFrameworkCore;
-using Router;
-using Telegram.Bot;
+using FloristAIBot;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-var builder = Host.CreateApplicationBuilder(args);
-
-/// <summary>
-/// Регистрирует контекст базы данных PostgreSQL с миграциями в сборке FloristAI.Infrastructure.
-/// </summary>
-var host = Environment.GetEnvironmentVariable("Host");
-var port = Environment.GetEnvironmentVariable("Port");
-var database = Environment.GetEnvironmentVariable("Database");
-var username = Environment.GetEnvironmentVariable("Username");
-var password = Environment.GetEnvironmentVariable("Password");
-
-var connectionString = $"Host={host};Port={port};Database={database};Username={username};Password={password}";
-
-builder.Services.AddDbContext<PostgresDbContext>(options =>
-    options.UseNpgsql(connectionString,
-        b => b.MigrationsAssembly("FloristAI.Infrastructure")));
-
-/// <summary>
-/// Регистрирует фоновой сервис для работы Telegram-бота.
-/// </summary>
-builder.Services.AddHostedService<BotWorker>();
-
-/// <summary>
-/// Регистрирует сервисы и адаптеры для обработки языков и маршрутизации сообщений.
-/// </summary>
-builder.Services.AddScoped<ILanguageService, LanguageService>();
-builder.Services.AddScoped<IMessageAdapter, LangTelegramAdapter>();
-builder.Services.AddScoped<AdapterRouter>();
-
-/// <summary>
-/// Регистрирует TelegramBotClient с токеном из конфигурации.
-/// </summary>
-builder.Services.AddSingleton<ITelegramBotClient>(provider =>
-{
-    var token = Environment.GetEnvironmentVariable("Bot_token");
-    return new TelegramBotClient(token);
-});
-
-/// <summary>
-/// Запускает приложение.
-/// </summary>
-await builder.Build().RunAsync();
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.UseStartup<Startup>();
+    })
+    .Build()
+    .Run();
