c0577c27c053592877845019c4060d9334c66b81
downh <downh@DESKTOP-AR9ARAH>
2025-07-19 19:37:21 +0300

    Добавлен сервис генерации реф.ссылки

---
+++ FloristAI.Service/Users/ReferralCode/IReferralService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloristAI.Application.Users.ReferralCode
+{
+    public interface IReferralService
+    {
+        string GetReferralLink(int Id);
+    }
+}
---
+++ FloristAI.Service/Users/ReferralCode/ReferralService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace FloristAI.Application.Users.ReferralCode
+{
+    public class ReferralService : IReferralService
+    {
+        private readonly IUserService _userService;
+
+        public ReferralService(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public string GetReferralLink(int Id)
+        {
+            string botName = "FLowerKisaBot";
+            // Генерация реферальной ссылки
+            return $"https://t.me/{botName}?start={Id}";
+
+        }
+
+
+    }
+}
