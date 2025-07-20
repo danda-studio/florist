37a2870df8f69d47a7c3d8b138a4c2f7844957e2
downh <downh@DESKTOP-AR9ARAH>
2025-07-20 23:45:15 +0300

    Добавил меню партнера и шаг "Реферальная ссылка"

---
+++ Adapter/PartnerMenuBuilder/PartnerStepReferralUrl.cs
@@ -0,0 +1,65 @@
+﻿using FloristAI.Adapter.Models;
+using FloristAI.Adapter.StepMenuBuilder;
+using FloristAI.Application.Language;
+using FloristAI.Application.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace FloristAI.Adapter.PartnerMenuBuilder
+{
+    public class PartnerStepReferralUrl : IStepMenuBuilder
+    {
+        private readonly IUserService _userService;
+        private readonly ILocalizationService _localizationService;
+        public PartnerStepReferralUrl(IUserService userService, ILocalizationService localizationService)
+        {
+            _userService = userService;
+            _localizationService = localizationService;
+        }
+
+        public string Step => "referal_url";
+
+        public async Task<MessageResult> BuildMenu(long chatId)
+        {
+            var user = await _userService.GetUser(chatId);
+            if (user == null)
+            {
+                return new MessageResult
+                {
+                    Text = _localizationService.GetString("UserNotFound", "ru"),
+                    ReplyMarkup = null
+                };
+            }
+
+            byte[] qrBytes = _userService.GetReferralQrCode(user.UserId);
+
+            var referralText = $"""
+            {_localizationService.GetString("Become_Form_Success", user.LanguageCode)}
+
+            {_localizationService.GetString("Referral_Link_Label", user.LanguageCode)} {_userService.GetReferralLink(user.UserId)}
+
+            {_localizationService.GetString("Referral_Description", user.LanguageCode)}
+            """;
+
+            var keyboard = new[]
+            {
+                new[] { InlineKeyboardButton.WithCallbackData(_localizationService.GetString("Button_Menu", user.LanguageCode), "role_menu:Partner") },
+            };
+
+            return new MessageResult
+            {
+                Text = referralText,
+                Photo = new PhotoContent
+                {
+                    ImageBytes = qrBytes,  // Передаём бинарные данные
+                    Description = referralText
+                },
+                ReplyMarkup = new InlineKeyboardMarkup(keyboard)
+            };
+        }
+    }
+}
